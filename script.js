00e9f83ce4c25f7f8a3e96393e526788d0c58d76
pityag-blrm20 <pityag-blrm20@pityag-blrm20.local>
2020-12-20 19:59:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+
+console.log(progress);
+console.log(prev);
+console.log(next);
+console.log(circles);
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    update();
+});
+
+prev.addEventListener("click", () => {
+    currentActive--;
+    console.log(currentActive);
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
+});
+
+const update = () => {
+    circles.forEach((circle, idx) => {
+        console.log(idx, circle);
+        if (idx < currentActive) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    });
+
+    const actives = document.querySelectorAll(".active");
+    console.log(actives);
+    console.log("actives len", actives.length);
+    console.log("circles len", circles.length);
+
+    progress.style.width =
+        ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+};
